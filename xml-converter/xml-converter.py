2219ed0cda6c4705bf41d26cb2076cacf40af9fe
kochuv <veronika.kochugova@gmail.com>
2020-05-14 13:34:32 +0300

    change accuracy & bw units

--- xml-converter/xml-converter.py
+++ xml-converter/xml-converter.py
@@ -22,29 +22,29 @@ def build_xml(row, step_id, config):
     step_dur = float(row['StepDuration[s]'])
     end_timestamp = start_timestamp + step_dur
     end_dt = datetime.fromtimestamp(end_timestamp)
-    result += " StartDate=\"" + str(start_dt.date()) + ' ' + str(start_dt.time()) + "\""
+    result += " StartDate=\"" + str(start_dt.date()) + ' ' + str(start_dt.time())[0:8] + "\""
     result += " StartTimestamp=\"" + str(start_timestamp) + "\""
-    result += " EndDate=\"" + str(end_dt.date()) + ' ' + str(end_dt.time()) + "\""
+    result += " EndDate=\"" + str(end_dt.date()) + ' ' + str(end_dt.time())[0:8] + "\""
     result += " EndTimestamp=\"" + str(end_timestamp) + "\""
     result += " operation=\"" + row['OpType'] + "\""
     result += " threads=\"" + str(int(row['Concurrency']) * int(row['NodeCount'])) + "\""
     result += " RequestThreads=\"" + row['Concurrency'] + "\""
     result += " clients=\"" + row['NodeCount'] + "\""
     result += " error=\"" + row['CountFail'] + "\""
-    result += " runtime=\"" + row['StepDuration[s]'] + "\""
+    result += " runtime=\"" + str(round(float(row['StepDuration[s]']),0)) + "\""
     result += " filesize=\"" + file_size + "\""
-    result += " tps=\"" + row['TPAvg[op/s]'] + "\""
+    result += " tps=\"" + str(round(float(row['TPAvg[op/s]']),2)) + "\""
     result += " tps_unit=\"" + "op/s" + "\""
-    result += " bw=\"" + row['BWAvg[MB/s]'] + "\""
+    result += " bw=\"" + str(round(float(row['BWAvg[MB/s]'])/1000000,2)) + "\""
     result += " bw_unit=\"" + "MB/s" + "\""
-    result += " latency=\"" + row['LatencyAvg[us]'] + "\""
+    result += " latency=\"" + str(round(float(row['LatencyAvg[us]']),2)) + "\""
     result += " latency_unit=\"" + "us" + "\""
     result += " latency_min=\"" + row['LatencyMin[us]'] + "\""
     result += " latency_loq=\"" + row['LatencyLoQ[us]'] + "\""
     result += " latency_med=\"" + row['LatencyMed[us]'] + "\""
     result += " latency_hiq=\"" + row['LatencyHiQ[us]'] + "\""
     result += " latency_max=\"" + row['LatencyMax[us]'] + "\""
-    result += " duration=\"" + row['DurationAvg[us]'] + "\""
+    result += " duration=\"" + str(round(float(row['DurationAvg[us]']),2)) + "\""
     result += " duration_unit=\"" + "us" + "\""
     result += " duration_min=\"" + row['DurationMin[us]'] + "\""
     result += " duration_loq=\"" + row['DurationLoQ[us]'] + "\""
@@ -52,31 +52,21 @@ def build_xml(row, step_id, config):
     result += " duration_hiq=\"" + row['DurationHiQ[us]'] + "\""
     result += " duration_max=\"" + row['DurationMax[us]'] + "\""
     result += "/>"
-    return result
+    return [ start_dt , result + "\n"]
 
 
 def build_pravega_xml(row, step_id, config):
-    """
-    TODO:
-    min50latency=?
-    max50latency=?
-    avg75latency=?
-    avg95latency=?
-    avg99latency=?
-    min99latency=?
-    max99latency=?
-    avg999latency=?
-    avg9999latency=?
-    """
     try:
         file_size = config["item"]["data"]["size"]
         segment_size = config["storage"]["driver"]["scaling"]["segments"]
         producers = config["storage"]["driver"]["threads"]
+        dt_iso = row['DateTimeISO8601']
+
     except KeyError as e:
         print(f"config.yaml for {step_id} is not pravega-driver specific. Don't use the key '-p'")
         print(f"KeyError: {e}")
         exit(1)
-
+    start_dt = datetime.strptime(dt_iso, '%Y-%m-%dT%H:%M:%S,%f')
     result = "<result id=\"" + step_id + "\""
     result += " operation=\"" + row['OpType'] + "\""
     result += " pods=\"" + row['NodeCount'] + "\""
@@ -97,7 +87,7 @@ def build_pravega_xml(row, step_id, config):
     result += " avg95latency=\"" + row['LatencyHiQ[us]'] + "\""
     result += " maxavglatency=\"" + row['LatencyMax[us]'] + "\""
     result += "/>"
-    return result
+    return [ start_dt , result + "\n"]
 
 
 def check_log_dir(path):
@@ -118,7 +108,7 @@ if __name__ == "__main__":
     args = set_and_get_options()
     logpath = args.logpath
     step_ids = get_step_ids(logpath)
-    result = "<result>\n"
+    result_dict = {}
 
     for step_id in step_ids:
         path_to_metric = f"{logpath}/{step_id}/metrics.total.csv"
@@ -129,8 +119,12 @@ if __name__ == "__main__":
             reader = csv.DictReader(csvfile, delimiter=',')
             for row in reader:
                 if args.pravega:
-                    result += build_pravega_xml(row, step_id, config) + "\n"
+                    result = build_pravega_xml(row, step_id, config)
                 else:
-                    result += build_xml(row, step_id, config) + "\n"
-    result += "</result>"
-    print(result)
+                    result = build_xml(row, step_id, config)
+                result_dict[result[0]] = result[1]
+    sorted_list = [v for k, v in sorted(result_dict.items(), key=lambda p: p[0], reverse=False)]
+    new_result = "<result>"
+    for i in range(0,len(sorted_list)):
+        new_result += sorted_list[i]
+    print(new_result + "</result>")
